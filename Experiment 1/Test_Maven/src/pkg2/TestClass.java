80b878617ff24a052fbd22c3d177ee2f90356bd1
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2023-01-24 14:13:12 +0530

    updating readme

--- Experiment 1/Test_Maven/src/pkg2/TestClass.java
+++ Experiment 1/Test_Maven/src/pkg2/TestClass.java
@@ -1,5 +1,7 @@
 package pkg2;
 
+import java.util.Scanner;
+
 import pkg1.MyClass;
 
 public class TestClass {
@@ -8,6 +10,9 @@ public class TestClass {
 		// TODO Auto-generated method stub
 		MyClass  obj = new MyClass();
 		System.out.println(obj.compute(11));
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		System.out.println(obj.compute(x));
 //		Output is 3, which is the number of set bits in 11 
 //		11 =(1011)base 2
 	}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# DevOps Build and Release Management Lab
+
 This repo is for Build and Relase Management Lab experiments
 
-Tools used: Apache Maven and Bazel
+Tools used: Apache Maven, JUnit Testing
