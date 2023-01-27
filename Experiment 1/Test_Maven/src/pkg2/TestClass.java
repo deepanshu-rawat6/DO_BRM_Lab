ff1edd73e2c637a796d5051f7ba69fc1b5e651f7
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2023-01-27 12:41:36 +0530

    adding Experiment_1 ; Simple Maven Commands

--- Experiment 1/Test_Maven/src/pkg2/TestClass.java
+++ Experiment 1/Test_Maven/src/pkg2/TestClass.java
@@ -9,10 +9,11 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyClass  obj = new MyClass();
-		System.out.println(obj.compute(11));
+		System.out.println("The number of bits in 10: " + obj.compute(11));
+		System.out.println("Enter a value to be checked: ");
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-		System.out.println(obj.compute(x));
+		System.out.println("The number of bits in " + x +": " +obj.compute(x));
 //		Output is 3, which is the number of set bits in 11 
 //		11 =(1011)base 2
 	}
